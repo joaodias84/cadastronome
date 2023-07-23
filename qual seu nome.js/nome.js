a750c26c47272789f39ee0ac9ae03595c4234492
João Paulo <105462675+joaodias84@users.noreply.github.com>
2023-07-23 17:40:58 -0300

    Cadastro de nome e sobrenome
    Cadastro simples de nome pessoal em Java script

---
+++ qual seu nome.js/nome.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual é o seu nome?");
+
+// Pedir o sobrenome da pessoa
+var sobrenome = prompt("Qual é o seu sobrenome?");
+
+// Concatenar o nome e sobrenome e mostrar na tela
+var nomeCompleto = nome + " " + sobrenome;
+console.log("Nome completo: " + nomeCompleto);
